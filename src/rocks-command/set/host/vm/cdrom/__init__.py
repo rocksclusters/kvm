f9469507f33c7de22e10f51b5a2c45304647fb74
Luca Clementi <luca.clementi@gmail.com>
2013-12-04 16:57:57 -0800

    commands: add command to mount a CDROM ISO path on a VM

---
+++ src/rocks-command/set/host/vm/cdrom/__init__.py
@@ -0,0 +1,125 @@
+#
+# 
+# @Copyright@
+# 
+# 				Rocks(r)
+# 		         www.rocksclusters.org
+# 		         version 5.6 (Emerald Boa)
+# 		         version 6.1 (Emerald Boa)
+# 
+# Copyright (c) 2000 - 2013 The Regents of the University of California.
+# All rights reserved.	
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+# 
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice unmodified and in its entirety, this list of conditions and the
+# following disclaimer in the documentation and/or other materials provided 
+# with the distribution.
+# 
+# 3. All advertising and press materials, printed or electronic, mentioning
+# features or use of this software must display the following acknowledgement: 
+# 
+# 	"This product includes software developed by the Rocks(r)
+# 	Cluster Group at the San Diego Supercomputer Center at the
+# 	University of California, San Diego and its contributors."
+# 
+# 4. Except as permitted for the purposes of acknowledgment in paragraph 3,
+# neither the name or logo of this software nor the names of its
+# authors may be used to endorse or promote products derived from this
+# software without specific prior written permission.  The name of the
+# software includes the following terms, and any derivatives thereof:
+# "Rocks", "Rocks Clusters", and "Avalanche Installer".  For licensing of 
+# the associated name, interested parties should contact Technology 
+# Transfer & Intellectual Property Services, University of California, 
+# San Diego, 9500 Gilman Drive, Mail Code 0910, La Jolla, CA 92093-0910, 
+# Ph: (858) 534-5815, FAX: (858) 534-7345, E-MAIL:invent@ucsd.edu
+# 
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS''
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# 
+# @Copyright@
+#
+# I was gonna go with the rocks set host vm disk="cdrom:/pathtoiso/file.ido,vda,virtio"
+# but then it seemed all to complicated for such a simple task. 
+#
+# So I optited for a much simpler solution.
+#
+#
+
+import os.path
+import rocks.commands
+
+
+class Command(rocks.commands.HostArgumentProcessor, rocks.commands.set.command):
+	"""
+	Add a CDROM image to the virtual machine specified.
+	The machine must be restarted to make this effective
+
+	<arg type='string' name='host' optional='0'>
+	One or more VM host names.
+	</arg>
+
+	<param type='string' name='cdrom'>
+	The path to the ISO image to use as a CDROM of the physical device 
+	path (/dev/cdrom)
+	if it is cdrom=none, it will remove the cdrom image from the current disks
+	</param>
+
+	<example cmd='set host vm cdrom compute-0-0-0 cdrom=/root/kernel-6.1-0.x86_64.disk1.iso'>
+	Mount the ISO /root/kernel-6.1-0.x86_64.disk1.iso as a CDROM
+	</example>
+
+	<example cmd='set host vm cdrom compute-0-0-0 cdrom=none'>
+	Remove the CDROM from node compute-0-0-0
+	</example>
+	"""
+
+	def run(self, params, args):
+
+		(cdrom,) = self.fillParams([ ('cdrom', ""), ])
+		if cdrom.lower() == 'none':
+			cdrom = ""
+		else:
+			# do the validation of the path
+			if not os.path.exists(cdrom):
+				self.abort('cdrom must be a valid path (%s)' % cdrom)
+
+			if cdrom[0] != '/':
+				self.abort('You must use an absolute path for the ISO')
+
+		hosts = self.getHostnames(args)
+		if len(hosts) != 1:
+			self.abort('must supply only one host')
+		host = hosts[0]
+
+		#
+		# update the DB first
+		#
+		rows = self.db.execute("""select id from nodes where
+		        name = '%s'""" % (host))
+
+		if rows == 1:
+		        vmnodeid, = self.db.fetchone()
+		else:
+		        self.abort('could not get node id for VM %s' % host)
+
+		rows = self.db.execute("""update vm_nodes set cdrom_path = '%s' 
+				where node = %d""" % (cdrom, vmnodeid))
+
+		#TODO add code to attach cdrom to running instances
+

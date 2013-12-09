f6c0666af54e7c2bab4af4ecd5f601e95b3b2a92
Luca Clementi <luca.clementi@gmail.com>
2013-12-09 15:35:15 -0800

    commands: charon: stop the service

---
+++ src/rocks-command/stop/service/charon/__init__.py
@@ -0,0 +1,104 @@
+#!/opt/rocks/bin/python
+#
+# @Copyright@
+# 
+# 				Rocks(r)
+# 		         www.rocksclusters.org
+# 		         version 5.6 (Emerald Boa)
+# 		         version 6.1 (Emerald Boa)
+# 
+# Copyright (c) 2000 - 2013 The Regents of the University of California.
+# All rights reserved.	
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+# 
+# 1. Redistributions of source code must retain the above copyright
+# notice, this list of conditions and the following disclaimer.
+# 
+# 2. Redistributions in binary form must reproduce the above copyright
+# notice unmodified and in its entirety, this list of conditions and the
+# following disclaimer in the documentation and/or other materials provided 
+# with the distribution.
+# 
+# 3. All advertising and press materials, printed or electronic, mentioning
+# features or use of this software must display the following acknowledgement: 
+# 
+# 	"This product includes software developed by the Rocks(r)
+# 	Cluster Group at the San Diego Supercomputer Center at the
+# 	University of California, San Diego and its contributors."
+# 
+# 4. Except as permitted for the purposes of acknowledgment in paragraph 3,
+# neither the name or logo of this software nor the names of its
+# authors may be used to endorse or promote products derived from this
+# software without specific prior written permission.  The name of the
+# software includes the following terms, and any derivatives thereof:
+# "Rocks", "Rocks Clusters", and "Avalanche Installer".  For licensing of 
+# the associated name, interested parties should contact Technology 
+# Transfer & Intellectual Property Services, University of California, 
+# San Diego, 9500 Gilman Drive, Mail Code 0910, La Jolla, CA 92093-0910, 
+# Ph: (858) 534-5815, FAX: (858) 534-7345, E-MAIL:invent@ucsd.edu
+# 
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS''
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# 
+# @Copyright@
+#
+#
+#
+
+import rocks.commands
+import os
+import re
+from rocks.commands.start.service.charon import pidfile 
+
+class Command(rocks.commands.stop.service.command):
+	"""
+	Stop charon service.
+	"""
+
+	def find_service(self, line):
+		l = line.split('\0')
+
+		if len(l) > 5 and l[0] == '/opt/rocks/bin/python' and \
+				l[1] == '/opt/rocks/bin/rocks' and \
+				l[2] == 'start' and l[3] == 'service' and \
+				l[4] == 'charon':
+			return 1
+
+		return 0
+
+
+	def run(self, params, args):
+
+		if os.path.exists(pidfile):
+			fd = open(pidfile)
+			pid = fd.read()
+			fd.close()
+			os.kill(int(pid), 9)
+			os.unlink(pidfile)
+			return
+		
+			
+		for p in os.listdir('/proc'):
+			if re.match('[0-9]+', p):
+				file = open('/proc/%s/cmdline' % p, 'r')
+				for line in file.readlines():
+					found = self.find_service(line)
+				file.close()
+
+				if found:
+					os.kill(int(p), 9)	
+					break
+
+

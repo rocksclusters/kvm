73f9be719e4b0d3cd7a188cdaf755f4b30610b95
Luca Clementi <luca.clementi@gmail.com>
2014-07-16 17:30:06 -0700

    new python packages lockfile and daemon
    python lockfile and python daemon are now used by airboss

---
+++ src/foundation-python-daemon/version.mk
@@ -0,0 +1,8 @@
+NAME               = foundation-python-daemon
+VERSION            = 1.5.5
+RELEASE            = 1
+TARBALL_POSTFIX    = tar.gz
+
+SRC_SUBDIR         = python-daemon
+
+
---
+++ src/foundation-python-lockfile/version.mk
@@ -0,0 +1,5 @@
+NAME               = foundation-python-lockfile
+VERSION            = 0.8
+RELEASE            = 1
+TARBALL_POSTFIX    = tar.gz
+

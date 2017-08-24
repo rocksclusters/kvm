431f01f1756684b6c4b4220178d9df8ff1726fb1
Philip Papadopoulos <ppapadopoulos@ucsd.edu>
2017-08-24 10:35:26 -0700

    Default memory is 2048 (larger installer). Default install on 7 is 'install notracker' (to fit within small mem footprint)

--- src/rocks-command/rocks/commands/add/host/vm/__init__.py
+++ src/rocks-command/rocks/commands/add/host/vm/__init__.py
@@ -232,6 +232,7 @@ import os
 import os.path
 import IPy
 
+import rocks
 import rocks.commands
 import rocks.vm
 from rocks.db.mappings.base import *
@@ -304,7 +305,7 @@ class Command(rocks.commands.HostArgumentProcessor, rocks.commands.add.command):
 
 	<param type='string' name='mem'>
 	The amount of memory in megabytes to assign to this VM.
-	The default is: 1024.
+	The default is: 2048.
 	</param>
 
 	<param type='string' name='cpus'>
@@ -638,11 +639,19 @@ class Command(rocks.commands.HostArgumentProcessor, rocks.commands.add.command):
 				'install vm' ] )
 
 		# HVMs boot just like real hardware
-		if virt_type == 'hvm':
+		if virt_type == 'hvm' and rocks.version_major == '7':
+			# No tracker is the default install for VMs
+			# Rocks 7 
 			self.command('set.host.installaction', [nodename, 
-					'install headless'])
+					'install notracker'])
+			self.command('add.host.attr',[nodename,'UseTracker','False'])
 			self.command('set.host.runaction', [nodename, 'os'])
 		#
+		if virt_type == 'hvm' and rocks.version_major != '7':
+			self.command('set.host.installaction', [nodename, 
+					'install'])
+			self.command('set.host.runaction', [nodename, 'os'])
+
 		# set the first boot state to 'install'
 		#
 		self.command('set.host.boot', [nodename, 'action=install'])
@@ -673,7 +682,7 @@ class Command(rocks.commands.HostArgumentProcessor, rocks.commands.add.command):
 				[('name', None),
 				('ip', None),
 				('subnet', 'private'),
-				('mem', 1024),
+				('mem', 2048),
 				('cpus', 1),
 				('slice', None),
 				('mac', None),
